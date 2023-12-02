fd3fd4af72801791c720f8b1d12251e11a64f0b6
Insia Farhan <k200265@nu.edu.pk>
2023-12-02 21:33:24 +0500

    Initial commit for Flask backend

---
+++ app.py
@@ -0,0 +1,12 @@
+# backend/app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello from Flask Backend!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
